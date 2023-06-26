2dc9c0b4d466d62415498ba38d336d889213a912
Nathan Barlow <nathanjbarlow@gmail.com>
2023-06-25 20:50:02 -0500

    Finish chapter 6

--- the-divine-cheese-code/src/the_divine_cheese_code/core.clj
+++ the-divine-cheese-code/src/the_divine_cheese_code/core.clj
@@ -1,5 +1,7 @@
 (ns the-divine-cheese-code.core
-  (:require [the-divine-cheese-code.visualization.svg :refer [xml]]))
+  (:require [clojure.java.browse :as browse]
+            [the-divine-cheese-code.visualization.svg :refer [xml]])
+  (:gen-class))
 
 (def heists [{:location "Cologne, Germany"
               :cheese-name "Archbishop Hildehold's Cheese Pretzel"
@@ -22,7 +24,23 @@
               :lat 41.90
               :lng 12.45}])
 
+(defn url
+  [filename]
+  (str "file:///"
+       (System/getProperty "user.dir")
+       "/"
+       filename))
+
+(defn template
+  [contents]
+  (str "<style>polyline { fill:none; stroke:#5881d8; stroke-width:3}</style>"
+       contents))
+
 (defn -main
   [& args]
-  (println (points heists)))
-
+  (let [filename "map.html"]
+    (->> heists
+         (xml 50 100)
+         template
+         (spit filename))
+    (browse/browse-url (url filename))))
